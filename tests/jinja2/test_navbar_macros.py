9fdbc7c93f86263bd9016dfcc54041e23c20e279
Paul Everitt <pauleveritt@me.com>
2018-08-26 13:22:12 -0400

    Start on navbar start.

--- tests/jinja2/test_navbar_macros.py
+++ tests/jinja2/test_navbar_macros.py
@@ -19,16 +19,40 @@ def context_brand():
     return dict(site=site, page=page)
 
 
+@pytest.fixture
+def context_start():
+    site = Site(
+        navbar=dict(
+            start=[
+                dict(
+                    css_class='documentation',
+                    accent='primary',
+                    icon='book',
+                    label='Documentation',
+                    label_narrow='Docs',
+                    href='documentation.html',
+                )
+            ]
+        )
+    )
+
+    return dict(site=site)
+
+
 @pytest.mark.parametrize(
-    'page',
-    [
-        ['brand.html', context_brand],
-    ],
-    indirect=True
+    'page', [['macros_brand.html', context_brand], ], indirect=True
 )
-def test_rendering(page):
+def test_brand(page):
     a = page.find('a', class_='bulmaio-brand')
     assert '/index.html' == a.attrs['href']
     logo = page.find('img', class_='bulmaio-logo-image')
     assert '_static/images/bulma-logo.png' == logo.attrs['src']
     assert 'Bulma Logo' == logo.attrs['alt']
+
+
+@pytest.mark.parametrize(
+    'page', [['macros_start.html', context_start], ], indirect=True
+)
+def test_start(page):
+    a = page.find_all('a', class_='navbar-item')
+    assert 1 == len(a)
