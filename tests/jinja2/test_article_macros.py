a236afb1c47e318076dcf47c0758933dfa427fee
Paul Everitt <pauleveritt@me.com>
2018-08-27 10:14:52 -0400

    Tests for heading. Clean up margin left and bottom on breadcrumbs.

--- tests/jinja2/test_article_macros.py
+++ tests/jinja2/test_article_macros.py
@@ -10,12 +10,20 @@ def context_no_breadcrumbs():
     )
     return dict(page=page)
 
+@pytest.fixture
+def context_no_subtitle():
+    page = Page(
+        docname='about',
+        title='Some Title'
+    )
+    return dict(page=page)
+
 
 @pytest.fixture
 def context_fullpage():
     page = Page(
         docname='about',
-        title='Some Page',
+        title='Some Title',
         subtitle='Some Subtitle',
         breadcrumbs=[
             dict(
@@ -63,3 +71,32 @@ def test_breadcrumbs(page):
     a1 = bcsli[1].find('a')
     assert '/documentation.html' == a1.attrs['href']
     assert 'Documentation' == a1.string.strip()
+
+
+
+@pytest.mark.parametrize(
+    'page',
+    [['macros_heading.html', context_no_subtitle], ],
+    indirect=True
+)
+def test_no_subtitle(page):
+    bcs = page.find_all('div', class_='subtitle')
+    assert 0 == len(bcs)
+
+
+@pytest.mark.parametrize(
+    'page',
+    [['macros_heading.html', context_fullpage], ],
+    indirect=True
+)
+def test_heading(page):
+    header = page.find('header')
+    assert ['bd-header'] == header.attrs['class']
+
+    # Title
+    title = header.find('h1', class_='title')
+    assert 'Some Title' == title.string.strip()
+
+    # Subtitle
+    subtitle = header.find('h1', class_='subtitle')
+    assert 'Some Subtitle' == subtitle.string.strip()
