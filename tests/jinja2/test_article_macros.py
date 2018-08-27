5a6a0c050f5544147c306bfc1dfd1d3f573859ef
Paul Everitt <pauleveritt@me.com>
2018-08-27 10:27:29 -0400

    Test for tabs.

--- tests/jinja2/test_article_macros.py
+++ tests/jinja2/test_article_macros.py
@@ -10,8 +10,18 @@ def context_no_breadcrumbs():
     )
     return dict(page=page)
 
+
 @pytest.fixture
 def context_no_subtitle():
+    page = Page(
+        docname='about',
+        title='Some Title'
+    )
+    return dict(page=page)
+
+
+@pytest.fixture
+def context_no_tabs():
     page = Page(
         docname='about',
         title='Some Title'
@@ -35,10 +45,27 @@ def context_fullpage():
                 href='/documentation.html',
                 is_active=True
             ),
+        ],
+        tabs=[
+            dict(
+                label='Tab 1',
+                href='tab1.html',
+                is_active=True
+            ),
+            dict(
+                label='Tab 2',
+                href='tab2.html',
+            ),
+            dict(
+                label='Tab 3',
+                href='tab3.html',
+            ),
         ]
     )
     return dict(page=page)
 
+
+# Breadcrumbs
 
 @pytest.mark.parametrize(
     'page',
@@ -73,6 +100,7 @@ def test_breadcrumbs(page):
     assert 'Documentation' == a1.string.strip()
 
 
+# Subtitle
 
 @pytest.mark.parametrize(
     'page',
@@ -90,7 +118,7 @@ def test_no_subtitle(page):
     indirect=True
 )
 def test_heading(page):
-    header = page.find('header')
+    header = page.find('header', class_='bd-header')
     assert ['bd-header'] == header.attrs['class']
 
     # Title
@@ -98,5 +126,43 @@ def test_heading(page):
     assert 'Some Title' == title.string.strip()
 
     # Subtitle
-    subtitle = header.find('h1', class_='subtitle')
+    subtitle = header.find('p', class_='subtitle')
     assert 'Some Subtitle' == subtitle.string.strip()
+
+
+# Tabs
+
+@pytest.mark.parametrize(
+    'page',
+    [['macros_tabs.html', context_no_tabs], ],
+    indirect=True
+)
+def test_no_tabs(page):
+    tabs = page.find_all('nav', class_='bd-tabs')
+    assert 0 == len(tabs)
+
+
+@pytest.mark.parametrize(
+    'page',
+    [['macros_tabs.html', context_fullpage], ],
+    indirect=True
+)
+def test_tabs(page):
+    tabs_wrapper = page.find('nav', class_='bd-tabs')
+    tabs = tabs_wrapper.find_all('li')
+    assert 3 == len(tabs)
+
+    # Tab 0
+    tab0 = tabs[0]
+    assert ['is-active'] == tab0.attrs['class']
+    tab0_a = tab0.find('a')
+    assert 'tab1.html' == tab0_a.attrs['href']
+    assert 'Tab 1' == tab0_a.string.strip()
+
+    # Tab 1
+    tab1 = tabs[1]
+    assert [''] == tab1.attrs['class']
+    tab1_a = tab1.find('a')
+    assert 'tab2.html' == tab1_a.attrs['href']
+    assert 'Tab 2' == tab1_a.string.strip()
+
