f8c73e46e19daf6793ee855a5be11a8efe77dccd
Paul Everitt <pauleveritt@me.com>
2018-08-28 17:44:22 -0400

    Published.

--- bulmaio_jinja2/page/sidebar/models.py
+++ bulmaio_jinja2/page/sidebar/models.py
@@ -1,5 +1,12 @@
+from bulmaio_jinja2.author.models import Author
 from bulmaio_jinja2.base_model import CustomBaseModel
 
 
+class SidebarPublished(CustomBaseModel):
+    published_date: str = None
+    published_time: str = None
+    author: Author = None
+
+
 class Sidebar(CustomBaseModel):
-    pass
+    published: SidebarPublished
---
+++ tests/jinja2/page/test_page_sidebar_published.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+
+import pytest
+from bulmaio_jinja2.page.sidebar.models import SidebarPublished
+from bulmaio_jinja2.utils import load_yaml
+
+sample = Path(__file__).parents[3] / 'bulmaio_jinja2' / 'sample'
+
+
+@pytest.fixture
+def context_no_sidebar_published():
+    sidebar_published = SidebarPublished()
+    return dict(sidebar_published=sidebar_published)
+
+
+@pytest.fixture
+def context_sidebar_published():
+    pages = load_yaml('pages', base_dir=sample)
+    page = pages[7]
+    published = page['sidebar']['published']
+    sidebar_published = SidebarPublished(**published)
+    return dict(sidebar_published=sidebar_published)
+
+
+@pytest.fixture
+def context_sidebar_draft():
+    pages = load_yaml('pages', base_dir=sample)
+    page = pages[7]
+    published = page['sidebar']['published']
+    del published['published_date']
+    sidebar_published = SidebarPublished(**published)
+    return dict(sidebar_published=sidebar_published)
+
+
+@pytest.mark.parametrize(
+    'page',
+    [['test_page_sidebar_published.html', context_no_sidebar_published], ],
+    indirect=True
+)
+def test_no_sidebar_published(page):
+    published = page.find_all('p', class_='bio-page-sidebar-published')
+    assert 0 == len(published)
+
+
+@pytest.mark.parametrize(
+    'page',
+    [['test_page_sidebar_published.html', context_sidebar_published], ],
+    indirect=True
+)
+def test_sidebar_published(page):
+    published = page.find_all('p', class_='bio-page-sidebar-published')
+    assert 1 == len(published)
+
+    published_date = page.find('span',
+                               class_='bio-page-sidebar-published-date')
+    e1 = 'Published\n                        on 2018-02-25'
+    assert e1 == published_date.string.strip()
+    published_time = page.find('span',
+                               class_='bio-page-sidebar-published-time')
+    e2 = '12:00'
+    assert e2 == published_time.string.strip()
+
+
+@pytest.mark.parametrize(
+    'page',
+    [['test_page_sidebar_published.html', context_sidebar_draft], ],
+    indirect=True
+)
+def test_sidebar_draft(page):
+    draft = page.find('p', class_='menu-label')
+    assert 'Draft' == draft.string.strip()
