e5ce44be414f8af279fbaa31385d4d27cd7455cf
Paul Everitt <pauleveritt@me.com>
2018-09-01 12:29:33 -0400

    All the YAML stuff is out of the handler. Still a lot of fake stuff in the adapter.

--- bulmaio_jinja2/footer/models.py
+++ bulmaio_jinja2/footer/models.py
@@ -1,8 +1,15 @@
 from bulmaio_jinja2.base_model import CustomBaseModel
 from bulmaio_jinja2.footer.links.models import FooterLinks
 from bulmaio_jinja2.footer.social.models import FooterSocial
+from bulmaio_jinja2.page.models import Page
+from bulmaio_jinja2.site.models import Site
 
 
 class Footer(CustomBaseModel):
     social: FooterSocial = None
     links: FooterLinks = None
+
+    def update(self, site: Site, page: Page):
+        """ Update hrefs and static paths from site/page data """
+
+        pass
